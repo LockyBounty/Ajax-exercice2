ee16eeff54cf5076023ac2c40ffe29513689a213
locky <bounthienglocky@gmail.com>
2018-12-05 13:17:33 +0100

    fonctionnal but need improvement

--- movie.js
+++ movie.js
@@ -1,57 +1,100 @@
+let API = "a49f54f4";
 
-let artisteAPI = "https://musicdemons.com/api/v1/artist";
 let input_value;
 
 let pushe = document.querySelector(".pushe");
+let remplir = document.querySelector(".remplissage");
 
+let titre = " ";
+// document.querySelector('#choper').value;
+let idFilm = "";
+let a = " ";
 
 //----------------- fonction prendre la valeur entree ------------
 let capcha = document.querySelector("#choper");
 
 
 function clicked() {
-    input_value = document.querySelector('#choper').value;
-    return input_value;
-   
+    titre = document.querySelector('#choper').value;
+
+    fetch(`http://www.omdbapi.com/?apikey=${API}&t=${titre}`)
+
+        .then(res => res.json()) //<--- on cree une variable qui va prendre les données du GET
+        .then(response => {
+
+            // console.log(response);
+
+            console.log(response.Plot);
+            a += `
+                <p>${response.Plot}</p>
+                `
+            remplir.innerHTML = a;
+
+        });
+
 }
 
+//TEST fetch image //
 
+// let myImage = document.querySelector("#testimg");
+
+// fetch('images/viagra.jpg')
+// .then(function(response) {
+//   return response.blob();
+// })
+// .then(function(myBlob) {
+//   let objectURL = URL.createObjectURL(myBlob);
+//   myImage.src = objectURL;
+// });
 
 //----------------- fin fonction --------------------
 
 
 
 
-fetch(artisteAPI, {
-    method : "GET", 
-    // body : JSON.stringify(),
-    // headers : {
-    //     "Content-Type" : "application/json"
-    // }
-    // --------> le body et le headers sont seulement necessaires avec la méthode POST <--------
-    // --------> le json vient automatiquement avec "parse" donc pas besoin de préciser <--------
+// fetch(API, {
+//     method : "POST", 
+//     body : JSON.stringify(),
+//     headers : {
+//         "Content-Type" : "application/json"
+//     }
+//     // --------> le body et le headers sont seulement necessaires avec la méthode POST <--------
+//     // --------> le json vient automatiquement avec "parse" donc pas besoin de préciser <--------
 
-}).then(res => res.json())   //<--- on cree une variable qui va prendre les données du GET
-.then(response=> {
-    
-    for (let i in response){   //<--- parcours toute la liste des artistes et va les afficher dans la console
-    // console.log(response[i]);   //<----|
+// }).then(res => res.json())   //<--- on cree une variable qui va prendre les données du GET
+// .then(response=> {
 
-    let myResponse = response[i].name;
+//     for (let i in response){   //<--- parcours toute la liste des artistes et va les afficher dans la console
+//     console.log(response[i]);   //<----|
+
+//     let myResponse = response[i].name;
 
 
 
-    console.log(myResponse);
-      if (myResponse == `${input_value}`){
-        document.querySelector('.remplissage').innerHTML = myResponse;
-        
-      }   
-}});
+//     console.log(myResponse);
+//       if (myResponse == `${input_value}`){
+//         document.querySelector('.remplissage').innerHTML = myResponse;
+
+//       }   
+// }});
+
+// fetch(`http://www.omdbapi.com/?apikey=${API}&t=${titre}`) 
+
+//     .then(res => res.json()) //<--- on cree une variable qui va prendre les données du GET
+//     .then(response => {
+
+//         console.log(response);
+
+//             if (response.Plot !== clicked){
+//                 console.log(response.Plot);
+//                 a +=
+//                 `
+//                 <p>${response.Plot}</p>
+//                 `
+//                 remplir.innerHTML=a;
+//             }
+//         });
 
 
 // capcha.addEventListener('keyup', clicked);
 pushe.addEventListener('click', clicked);
-
-
-
-
